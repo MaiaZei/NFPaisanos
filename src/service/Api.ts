47bca7b2fab0040afb9dda2eb4339015e2da47ee
Maia Ubuntu SSD <maiazeitoune@gmail.com>
2023-03-02 20:06:54 -0300

    changing proxy

--- src/service/Api.ts
+++ src/service/Api.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export default () => {
   return axios.create({
-    baseURL: 'http://localhost:3000',
+    baseURL: 'https://nf-paisanos-iej5.vercel.app',
     headers: {
       apiKey: 'aZmoOYPTSUCSdHzGhAZ4dP2SuUQ11DHZ7lwZBzHOb5o=',
     },
